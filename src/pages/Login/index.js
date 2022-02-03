d2d3af60faf5be0c0b4d51c548bb3b33809c2108
karenpoveda29 <karenpoveda29@gmail.com>
2022-02-03 16:41:16 -0500

    feat: add login form mark up

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,35 @@
 const Login = () => {
-  return <p>Soy Login page</p>;
-}
+  return (
+    <form>
+      <did>
+        <label htmlFor="email">
+          Email
+          <span className="require">*</span>
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          required
+        />
+      </did>
+
+      <div>
+        <label htmlFor="password">
+          Contraseña
+          <span className="require">*</span>
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          required
+        />    
+      </div>
+
+      <button type="submit">Ingresar</button>
+    </form>
+  );
+};
 
 export default Login;
