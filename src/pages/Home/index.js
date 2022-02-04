700c30baf87f32a51f041468ee49de7035bf81ef
karenpoveda29 <karenpoveda29@gmail.com>
2022-02-04 00:04:24 -0500

    feat: add pagination to homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,14 +8,25 @@ const POSTER_URL = "https://image.tmdb.org/t/p/w185/";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    getMovies({page: 1}).then(movies => setMovies(movies))
-  }, [])
+    getMovies({ page }).then(movies => setMovies(movies))
+  }, [page])
   
+  const handlePreviousPage = () => {
+    setPage(page => page - 1)
+  };
+  const handleNextPage = () => {
+    setPage(page => page + 1)
+  };
+
 
   return (
     <div>
       <h2>Top Rated Movies</h2>
+      <button type="button" disabled={page === 1} onClick={handlePreviousPage}>Previous</button>
+      <button type="button" onClick={handleNextPage}>Next</button>
       <ul>
         {movies?.map(movie => <li key={movie.id}>
           <img src={`${POSTER_URL}${movie.poster_path}`} alt={movie.title}/>
