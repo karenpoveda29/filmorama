3d79df9339fb1ae24b604a585371d1a707047111
karenpoveda29 <karenpoveda29@gmail.com>
2022-02-05 18:17:44 -0500

    feat: add clickable icon in navigation bar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,21 +2,25 @@ import { Link } from "react-router-dom";
 import { BsFillBookmarkHeartFill, BsFillCameraReelsFill } from "react-icons/bs";
 import "./navigation.css";
 
- const Navigation = () => {
+const Navigation = () => {
   return (
     <nav className="navbar">
       <ul className="navbar__list">
-        <li className="navbar__list-item">
-          <BsFillCameraReelsFill color="#ddd1ff" size="1.5rem"/>
-          <Link className="navbar__link" to="/">Home</Link>
-        </li>
-        <li className="navbar__list-item">
-          <BsFillBookmarkHeartFill className="navbar__icon-link" />
-          <Link className="navbar__link" to="/favorites">Favorites</Link>
-        </li>
+        <Link className="navbar__link" to="/">
+          <li className="navbar__list-item">
+            <BsFillCameraReelsFill className="navbar__icon" />
+            <span>Home</span>
+          </li>
+        </Link>
+        <Link className="navbar__link" to="/favorites">
+          <li className="navbar__list-item">
+            <BsFillBookmarkHeartFill className="navbar__icon" />
+            <span>Favorites</span>
+          </li>
+        </Link>
       </ul>
     </nav>
   );
 };
 
-export default Navigation; 
+export default Navigation;
