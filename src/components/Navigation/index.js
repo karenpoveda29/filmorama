33458380a75cc12b3c6c37d92cc53bc74683ce5e
karenpoveda29 <karenpoveda29@gmail.com>
2022-02-05 00:06:36 -0500

    feat: add styles to navigation bar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
- 
+import { BsFillBookmarkHeartFill, BsFillCameraReelsFill } from "react-icons/bs";
+import "./navigation.css";
+
  const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/favorites">Favorites</Link></li>
+    <nav className="navbar">
+      <ul className="navbar__list">
+        <li className="navbar__list-item">
+          <BsFillCameraReelsFill color="#ddd1ff" size="2rem"/>
+          <Link className="navbar__link" to="/">Home</Link>
+        </li>
+        <li className="navbar__list-item">
+          <BsFillBookmarkHeartFill color="#ddd1ff" size="2rem"/>
+          <Link className="navbar__link" to="/favorites">Favorites</Link>
+        </li>
       </ul>
     </nav>
   );
