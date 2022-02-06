3fca1542e3de19466d89d54b4220eb8c4d577f62
karenpoveda29 <karenpoveda29@gmail.com>
2022-02-06 17:47:59 -0500

    feat: add styles to pagination

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,13 +1,16 @@
 import "./pagination.css";
 
+//Icons
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+
 const Pagination = ({page, onPreviousPage, onNextPage}) => {
   return (
     <div className="pagination">
-      <button type="button" disabled={page === 1} onClick={onPreviousPage}>
-        Previous
+      <button type="button" disabled={page === 1} className="pagination__btn pagination__btn-previous" onClick={onPreviousPage}>
+        <BsArrowLeft className="pagination__btn-icon"/>
       </button>
-      <button type="button" onClick={onNextPage}>
-        Next
+      <button type="button" className="pagination__btn" onClick={onNextPage}>
+        <BsArrowRight className="pagination__btn-icon"/>
       </button>
     </div>
   );
